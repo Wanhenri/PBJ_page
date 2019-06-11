01cefec59e5de6628da888bc63d6839678792697
wanhenri <slipkorn.jk@gmail.com>
2019-06-11 17:38:53 -0300

    criando o component de title acomodacao

---
+++ app/src/components/TitleAcomodacao/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import {Title} from "./styles";
+
+const TitleAcomodacao = () => (
+    <Title>ACOMODAÇÕES COM QUALIDADE E CONFORTO</Title>
+);
+
+export default TitleAcomodacao;
+
