d7764dd53ccdb5f60a6a4ff45e6236e4c9ecdc10
wanhenri <slipkorn.jk@gmail.com>
2019-06-11 17:40:16 -0300

    acrescentando paramentos no index da Home

--- app/src/pages/Home/index.js
+++ app/src/pages/Home/index.js
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { Fragment } from "react";
 
-import Title from "../../objects/Title";
+import AcomodacaoPousada from "../../components/acomodacao";
 
-const Home = () => (<Title>TESTE</Title>);
+import TitleAcomodacao from "../../components/TitleAcomodacao"
+import TitleAjuda from "../../components/TitleAjuda"
+import TitleNovidade from "../../components/TitleNovidade"
+import SubtitlePousada from "../../components/SubtitlePousada";
+
+const Home = () => (
+    <Fragment>
+        <TitleAjuda>COMO PODEMOS TE AJUDAR?</TitleAjuda>
+        
+        <TitleAcomodacao>ACOMODAÇÕES COM QUALIDADE E CONFORTO</TitleAcomodacao>
+        
+        <SubtitlePousada>
+            Acomodações com todos os confortos de sua casa e muito mais. 
+            Venha conferir       
+        </SubtitlePousada>
+
+        <AcomodacaoPousada />
+
+        <TitleNovidade>QUER RECEBER NOVIDADES DOS EVENTOS E DISPONIBILIDADES DE VAGA?</TitleNovidade>
+
+        <SubtitlePousada>Coloque seu nome e-mail abaixo</SubtitlePousada>
+
+    
+    </Fragment>
+);
 
 export default Home;
