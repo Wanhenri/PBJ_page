23600a424f5fba2f5d5a881915a5202ee9f4d860
wanhenri <slipkorn.jk@gmail.com>
2019-06-11 12:25:30 -0300

    adicionao os styles padrão

---
+++ app/src/styles/settings/spacing.js
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+
+const Spacing = createGlobalStyle`
+    :root {
+        --spacing-small:1rem;
+        --spacing-big: 4rem;
+    }    
+    @media (max-height:700px){
+        :root{
+        --spacing-base:0.5rem;
+        --spacing-big: 2rem;
+        }
+    }
+`;
+
+export default Spacing;
