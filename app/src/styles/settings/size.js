d07587e167fde4d075a1c2895beff460b35d630e
wanhenri <slipkorn.jk@gmail.com>
2019-06-11 17:41:20 -0300

    modificando o size medium e very big

--- app/src/styles/settings/size.js
+++ app/src/styles/settings/size.js
@@ -6,8 +6,8 @@ const Size = createGlobalStyle `
         --radius-small: 4px;
         --size-base:10px;
         --size-small:1.5em;
-        --size-medium:1.8em;
-        --size-very-big:5em;
+        --size-medium:1.1em;
+        --size-very-big:1.5em;
     }
     @media (max-height:700px) {
         :root{
