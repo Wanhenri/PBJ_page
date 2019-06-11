2c3e8811a21fbf4743ad9c10c7ff9fa86d2cf57a
wanhenri <slipkorn.jk@gmail.com>
2019-06-11 17:39:33 -0300

    criando o component de subtitle da Pousada

---
+++ app/src/components/SubtitlePousada/index.js
@@ -0,0 +1,8 @@
+import styled from "styled-components";
+
+const SubtitlePousada = styled.h2 `
+    color: var(--color-second);
+    font-size: var(--size-medium);
+`;
+
+export default SubtitlePousada;
