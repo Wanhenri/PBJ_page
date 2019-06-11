711caaeaec81005a6ddd0dcd57b51350407e0f3f
wanhenri <slipkorn.jk@gmail.com>
2019-06-11 17:34:40 -0300

    criação do component acomodacao

---
+++ app/src/components/acomodacao/index.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const AcomodacaoPousada = styled.div `
+    display:inline-block;
+    background-color: #3f3d56;
+    width:430px;
+    height:346px;
+`;
+
+export default AcomodacaoPousada;
