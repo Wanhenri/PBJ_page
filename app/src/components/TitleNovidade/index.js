105def18d00816c67dc95c738b629a9306ef342f
wanhenri <slipkorn.jk@gmail.com>
2019-06-11 17:36:39 -0300

    criando o component do title novidades

---
+++ app/src/components/TitleNovidade/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import {Title} from "./styles";
+
+const TitleNovidade = () => (
+    <Title>QUER RECEBER NOVIDADES DOS EVENTOS E DISPONIBILIDADES DE VAGA?</Title>
+);
+
+export default TitleNovidade;
