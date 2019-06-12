949931e6b2a803b64b4dd1f1c7c2c264a762fc2d
wanhenri <slipkorn.jk@gmail.com>
2019-06-12 11:00:01 -0300

    voltando a configuração anterior

--- app/src/pages/Home/index.js
+++ app/src/pages/Home/index.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react";
+import React from "react";
 
 import FormLead from "../../components/FormLead";
 
@@ -8,26 +8,25 @@ import AcomodacaoPousada from "../../components/acomodacao";
 
 import TitleAcomodacao from "../../components/TitleAcomodacao"
 import TitleAjuda from "../../components/TitleAjuda"
-import TitleNovidade from "../../components/TitleNovidade"
+
 import SubtitlePousada from "../../components/SubtitlePousada";
 
 const Home = () => (
+    
     <Wrapper>
         <Section>
             <TitleAjuda>COMO PODEMOS TE AJUDAR?</TitleAjuda>
-                
             <TitleAcomodacao>ACOMODAÇÕES COM QUALIDADE E CONFORTO</TitleAcomodacao>
-                
             <SubtitlePousada>
                 Acomodações com todos os confortos de sua casa e muito mais. 
                 Venha conferir       
             </SubtitlePousada>
-        
+
             <AcomodacaoPousada />
         
             <FormLead />                  
-            
-        </Section>            
+
+        </Section>  
     </Wrapper>
 );
 
