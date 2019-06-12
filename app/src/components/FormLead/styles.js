be7526a1f339cf0b6a92a7524583eabb95e43cbe
wanhenri <slipkorn.jk@gmail.com>
2019-06-12 11:03:07 -0300

    atualiado

--- app/src/components/FormLead/styles.js
+++ app/src/components/FormLead/styles.js
@@ -5,7 +5,9 @@ import InputPousada from "../../objects/InputPousada";
 import TextPousada from '../../objects/TextPousada';
 
 export const Form = styled.form`
+
     position:relative;
+    
     &> ${TextPousada}{
         margin-bottom: var(--spacing-small);
     }
